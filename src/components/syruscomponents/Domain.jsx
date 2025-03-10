eaa83147dfc27361b648091dd6a59a1b411333f1
Gautam-04 <2022.gautam.rai@ves.ac.in>
2025-03-10 20:44:39 +0530

    Update Domain component content for clarity and enhance button height in styles
    Signed-off-by: Gautam-04 <2022.gautam.rai@ves.ac.in>

--- src/components/syruscomponents/Domain.jsx
+++ src/components/syruscomponents/Domain.jsx
@@ -17,13 +17,13 @@ function Domain() {
       </div> */}
       <div className="newCard">
         <div className="newCard_content">
-          <h1 className="genAifintech">Agentic Ai</h1>
-          <p>Agentic AI enables autonomous decision-making by leveraging advanced generative AI models that learn, adapt, and act independently. It streamlines complex workflows and enhances automation across industries.</p>
+          <h1 className="genAifintech">Agentic Ai and Gen Ai For Fintech</h1>
+          <p>Agentic AI and Gen AI are transforming Fintech by enabling intelligent automation, predictive analytics, and autonomous decision-making. These advanced AI models enhance fraud detection, risk assessment, and customer experience, driving efficiency and innovation in financial services.</p>
         </div>
-        <div className="newCard_content">
+        {/* <div className="newCard_content">
           <h1 className="genAifintech">Gen Ai For Fintech</h1>
           <p>Gen AI for Fintech revolutionizes financial services by integrating advanced AI models to enhance risk assessment, automate trading strategies, detect fraud, and provide personalized experiences.</p>
-        </div>
+        </div> */}
       </div>
     </section>
   );
